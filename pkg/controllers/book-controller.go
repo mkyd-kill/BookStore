b61d72c89a1e94af629ca56d1f7130edc6532b61
mkyd-kill <romeomureithi@gmail.com>
2022-08-21 20:47:13 +0300

    Controller functions

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,39 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/mkyd-kill/Bookstore/pkg/utils"
+	"github.com/mkyd-kill/Bookstore/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+
+	// converting to json
+	response, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while Parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+
+	// sending a response to the user
+	response, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
