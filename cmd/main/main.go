49f779602df9167eb1b9a328f6f0f9a2745487ad
mkyd-kill <romeomureithi@gmail.com>
2022-08-21 20:16:19 +0300

    Registered the routes

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	// absolute path routes importation
+	"github.com/mkyd-kill/Bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	// routers handler
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9000", r))
+}
