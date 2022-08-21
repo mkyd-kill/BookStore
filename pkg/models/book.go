c1290674376aff93a1a893ea4e3f88819fbe42ed
mkyd-kill <romeomureithi@gmail.com>
2022-08-21 20:10:50 +0300

    Database models

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/mkyd-kill/Bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `gorm: ""json: "name" `
+	Author string `json: "author" `
+	Publication string `json: "publication" `
+}
+
+// database initialisation
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
